b19a5b80f09ce40ce5380d0f1bd683744f7fd9e9
Michal Domonkos <mdomonko@redhat.com>
2019-07-04 19:09:04 +0200

    Refactor

--- model.py
+++ model.py
@@ -5,12 +5,9 @@ class List(object):
     SCROLLABLE_LEFT = 1
     SCROLLABLE_RIGHT = 2
 
-    def __init__(self, items=None, size=5):
-        if items is None:
-            items = []
-        self._items = items
-        self.size = size
-        self.cursor = 0
+    def __init__(self):
+        self._items = []
+        self.size = 0
         self.selected = False
 
     @property
@@ -21,6 +18,7 @@ class List(object):
     def size(self, val):
         self._size = val
         self._viewport = (0, val)
+        self.cursor = 0
 
     @property
     def cursor(self):
@@ -31,25 +29,21 @@ class List(object):
         val = max(0, min(val, len(self) - 1))
         self._cursor = val
         delta = val - self._viewport[0]
-        if delta >= self.size:
-            self._viewport = (val - self.size + 1, val + 1)
+        if delta >= self._size:
+            self._viewport = (val - self._size + 1, val + 1)
         elif delta < 0:
-            self._viewport = (val, val + self.size)
+            self._viewport = (val, val + self._size)
 
     @property
-    def selected(self):
+    def item(self):
         if not self:
             return None
         return self._items[self.cursor]
 
-    @selected.setter
-    def selected(self, val):
-        self._selected = val
-
     def __iter__(self):
         start, end = self._viewport
         for i, item in enumerate(self._items[start:end]):
-            item.selected = self._selected and (i + start) == self.cursor
+            item.selected = self.selected and (i + start) == self.cursor
             yield item
 
     def __len__(self):
@@ -75,24 +69,27 @@ class Card(object):
 
     def paint(self, win, y, x):
         reverse = curses.A_REVERSE if self.selected else 0
-        if reverse:
-            win.addstr(y, x, '▇' * self.width)
+        win.addstr(y, x, ' ' * self.width, reverse)
         win.addstr(y + 1, x, self.name.center(self.width), reverse)
         for i in range(self.height - 3):
             win.addstr(y + 2 + i, x, ' ' * self.width, reverse)
         if reverse:
-            win.addstr(y + (self.height - 1), x, '▁' * self.width,
+            win.addstr(y + (self.height - 1), x, ' ' * self.width,
                        curses.A_REVERSE)
 
 
 class Column(List):
-    def __init__(self, name, width=25, size=5):
+    def __init__(self, name):
+        super().__init__()
         self.name = name
-        self.width = width
-        super().__init__(size=size)
+
+    @property
+    def width(self):
+        if not self:
+            return 0
+        return self._items[0].width
 
     def add(self, card):
-        card.width = self.width
         self._items.append(card)
 
     def paint(self, win, y, x, title='%s'):
@@ -101,27 +98,40 @@ class Column(List):
         win.addstr(cur, x, title.center(self.width))
         cur += 1
         if self.scrollable & List.SCROLLABLE_LEFT:
-            win.addstr(cur, x, '▲'.center(self.width))
+            win.addstr(cur, x, '^^^'.center(self.width))
         cur += 1
         for card in self:
             card.paint(win, cur, x)
             cur += card.height
         if self.scrollable & List.SCROLLABLE_RIGHT:
-            win.addstr(cur, x, '▼'.center(self.width))
+            win.addstr(cur, x, 'vvv'.center(self.width))
 
 
 class Board(List):
-    def __init__(self, name, win, y=0, x=0, colwidth=25):
+    def __init__(self, name, win, y=0, x=0):
+        super().__init__()
         self.name = name
         self.win = win
         self.y = y
         self.x = x
-        self.colwidth = colwidth
-        super().__init__()
         self.selected = True
 
+    @property
+    def colwidth(self):
+        if not self:
+            return 0
+        return self._items[0].width
+
+    @property
+    def cardheight(self):
+        if not self:
+            return 0
+        col = self._items[0]
+        if not col:
+            return 0
+        return col._items[0].height
+
     def add(self, column):
-        column.width = self.colwidth
         self._items.append(column)
 
     def right(self):
@@ -131,37 +141,34 @@ class Board(List):
         self.cursor -= 1
 
     def down(self):
-        if self.selected is not None:
-            self.selected.cursor += 1
+        if self.item is not None:
+            self.item.cursor += 1
 
     def up(self):
-        if self.selected is not None:
-            self.selected.cursor -= 1
+        if self.item is not None:
+            self.item.cursor -= 1
 
     def resize(self):
-        maxy, maxx = self.win.getmaxyx()
-        self.size = min(maxx // self.colwidth, len(self))
-        colheight = (maxy - 3) // 4
-        for column in self:
-            column.size = colheight
-            for card in column:
-                card.height = 4
-
-        self.x = (maxx - (self.size * self.colwidth)) // 2
+        my, mx = self.win.getmaxyx()
+        self.size = min(mx // self.colwidth, len(self))
+        colsize = (my - 3) // self.cardheight
+        for column in self._items:
+            column.size = colsize
+        self.x = (mx - (self.size * self.colwidth)) // 2
 
     def paint(self):
         self.win.erase()
-        maxy, maxx = self.win.getmaxyx()
-        if maxy < 4 + 4 or maxx < self.colwidth + 1:
+        my, mx = self.win.getmaxyx()
+        if my < self.cardheight + 4 or mx < self.colwidth + 1:
             return
         cur = self.x
         items = list(self)
         for i, column in enumerate(items):
             title = '%s'
             if i == 0 and self.scrollable & List.SCROLLABLE_LEFT:
-                title = '◀ ' + title
+                title = '< ' + title
             if i == len(items) - 1 and self.scrollable & List.SCROLLABLE_RIGHT:
-                title += ' ▶'
+                title += ' >'
             column.paint(self.win, self.y, cur, title)
             cur += column.width
         self.win.refresh()
